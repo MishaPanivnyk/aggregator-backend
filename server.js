ce52f1393d952e62e2230f1ceee509628304124e
diwmix <sanuaburdun15@gmail.com>
2024-02-24 18:31:25 +0200

    plus protection

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 import app from './app.js'
 import mongoose from 'mongoose';
-const DBUrl = "mongodb+srv://Diwmix:cRK1TqdoRl1IJUsZ@aggregator.conqg6j.mongodb.net/Agreggator?retryWrites=true&w=majority&appName=Aggregator";
+import { configDotenv } from 'dotenv';
+
+configDotenv()
 
 async function start() {
     try {
-      await mongoose.connect(DBUrl);
+      await mongoose.connect(process.env.DBURL);
       console.log("BD was started successfully")
   
-      app.listen(3000, () => {
-        console.log("Server running. Use our API on port: 3000")
+      app.listen(process.env.PORT || 3000, () => {
+        console.log(`Server running. Use our API on port: ${process.env.PORT}`)
       })
     } catch (error) {
       console.log(error);
