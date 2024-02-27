50ced506c1a7c7ac7e2c6fb01d0b837e3b1fac15
diwmix <sanuaburdun15@gmail.com>
2024-02-27 23:46:39 +0200

    Images

--- routes/api/blogs.js
+++ routes/api/blogs.js
@@ -1,26 +1,27 @@
 import express from 'express';
 import Blog from '../../models/Blog.js'
-import multer from 'multer'
+import cloudinary from 'cloudinary'
+// html
+import { dirname } from 'path'
 import path from 'path'
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads/blogImg")
-  },
-  filename: (req, file, cb) => {
-    console.log(file)
-    cb(null, Date.now() + path.extname(file.originalname));
-  }
-})
-
-const upload = multer({ storage: storage })
+import { fileURLToPath } from 'url';
+const __dirname = dirname(fileURLToPath(import.meta.url));
+// html
+cloudinary.config({
+  cloud_name: 'vdoma',
+  api_key: '649237283136974',
+  api_secret: '8HkqvdO2LrTH3v2IAy7VVfmqEuk'
+});
 
 const router = express.Router();
 
 router.get('/blogs', async (req, res) => {
-  try {
+
+  try {  
+    const htmlFilePath = path.join(__dirname, '../../index.html');
+    res.sendFile(htmlFilePath);
     const blogs = await Blog.find();
-    res.json(blogs);
+    //res.json(blogs);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
@@ -28,18 +29,18 @@ router.get('/blogs', async (req, res) => {
 
 
 
-router.post('/blogs', upload.single("image"), async (req, res) => {
-  const blog = new Blog({
-    title: req.body.title,
-    content: req.body.content,
-    author: req.body.author,
-    imageUrl: req.file ? req.file.filename : null,
-    category: req.body.category
-  });
-  try {
+router.post('/blogs', async (req, res) => {
+  try {  
+    const result = await cloudinary.uploader.upload(req.files.image.path); 
+    const blog = new Blog({
+      title: req.body.title,
+      content: req.body.content,
+      author: req.body.author,
+      imageUrl: result.secure_url,
+      category: req.body.category
+    });
     const newBlog = await blog.save();
     res.status(201).json(newBlog);
-    res.redirect('/blogs')
   } catch (err) {
     res.status(400).json({ message: err.message });
   }
