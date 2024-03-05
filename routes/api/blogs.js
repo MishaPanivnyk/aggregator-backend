5a2e672c3f3452c5b851b7c1b443ed03a8527d9c
Misha Panivnyk <panivnykm@gmail.com>
2024-03-05 16:09:39 +0200

    add cloudinry/blogs

--- routes/api/blogs.js
+++ routes/api/blogs.js
@@ -1,35 +1,48 @@
-import express from 'express';
-import Blog from '../../models/Blog.js'
-import path from 'path'
+import express from "express";
+import Blog from "../../models/Blog.js";
+import cloudinary from "cloudinary";
+import multer from "multer";
 
 const router = express.Router();
 
-router.get('/blogs', async (req, res) => {
+cloudinary.config({
+  cloud_name: "dm5qpgyuo",
+  api_key: "892697833836662",
+  api_secret: "RjMs-Co98PSDuzPNbWK3JPKwx60",
+});
+
+const upload = multer({ dest: "uploads/" });
+
+router.get("/blogs", async (req, res) => {
   try {
     const blogs = await Blog.find();
     res.json(blogs);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
-})
+});
 
-router.post('/blogs', upload.single("image"), async (req, res) => {
-  const blog = new Blog({
-    title: req.body.title,
-    content: req.body.content,
-    author: req.body.author,
-    imageUrl: req.body.imageUrl,
-    category: req.body.category
-  });
+router.post("/blogs", upload.single("image"), async (req, res) => {
   try {
+    const result = await cloudinary.uploader.upload(req.file.path);
+
+    const blog = new Blog({
+      title: req.body.title,
+      content: req.body.content,
+      author: req.body.author,
+      imageUrl: result.secure_url,
+      category: req.body.category,
+    });
+
     const newBlog = await blog.save();
+
     res.status(201).json(newBlog);
   } catch (err) {
     res.status(400).json({ message: err.message });
   }
-})
+});
 
-router.get('/blogs/:id', async (req, res) => {
+router.get("/blogs/:id", async (req, res) => {
   const id = req.params.id;
   try {
     const blog = await Blog.findById(id);
@@ -42,7 +55,7 @@ router.get('/blogs/:id', async (req, res) => {
   }
 });
 
-router.delete('/blogs/:id', async (req, res) => {
+router.delete("/blogs/:id", async (req, res) => {
   const id = req.params.id;
   try {
     const deletedBlog = await Blog.findByIdAndDelete(id);
@@ -55,4 +68,4 @@ router.delete('/blogs/:id', async (req, res) => {
   }
 });
 
-export default router
+export default router;
