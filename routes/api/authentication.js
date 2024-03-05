61dc082c061c8b2ee465b6836a5f2cfbc634c19b
Misha Panivnyk <panivnykm@gmail.com>
2024-03-05 17:08:36 +0200

    fix problem/path-cloudinary

--- routes/api/authentication.js
+++ routes/api/authentication.js
@@ -1,11 +1,9 @@
-import express from 'express';
-import mongoose from 'mongoose';
+import express from "express";
+import mongoose from "mongoose";
 const router = express.Router();
 
-router.post('/registration', async (req, res) => {
-})
+router.post("/registration", async (req, res) => {});
 
-router.post('/login', async (req, res) => {
-})
+router.post("/login", async (req, res) => {});
 
-export default router
+export default router;
